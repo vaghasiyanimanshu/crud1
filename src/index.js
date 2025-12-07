109ae20b219896e6a54d9d9345daa44248320ab9
Vaghasiya Nimanshu <vaghasiyanimanshu@gmail.com>
2025-12-07 21:38:55 +0530

    fix css path

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import View from './view';
 import Insert from './insert';
 import Login from './login';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Update from './update';
 // import './index.css';
-// import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
